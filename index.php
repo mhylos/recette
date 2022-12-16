77c091d26765ba5b50093c2f34b2ad377e4090ed
Camilo-Abn <camilovalenzuela@live.cl>
2022-12-15 23:53:02 -0300

    todo pagination

--- index.php
+++ index.php
@@ -58,21 +58,7 @@
                         </div>
                         <div class="pagination-container d-flex flex-row justify-content-center">
                             <ul class="pagination">
-                                <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                </li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
